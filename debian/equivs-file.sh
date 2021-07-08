1c8e443244e38a826e60337c58aaf32b2b9c4859
Jesse Larrew <jesse.larrew@amd.com>
2021-07-08 18:13:48 +0000

    control: fix formatting of files to install
    Signed-off-by: Jesse Larrew <jesse.larrew@amd.com>

--- debian/equivs-file.sh
+++ debian/equivs-file.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-nr_files=$#
-
 #echo -n "Files:"
 
 for file in $@; do
 	[ ! -f ${file} ] && continue
 
 	dir=$(dirname ${file})
-	echo -n " ${file} ${dir#..}"
-
-	let nr_files--
-
-	if [ "${nr_files}" -gt 0 ]; then
-		echo ","
-	else
-		echo
-	fi
+	echo " ${file} ${dir#..}"
 done
